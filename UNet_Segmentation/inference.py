3c8a9ed3b504f42a8b3d78e506f953dc2e33ba7c
Kamran Janjua <kamranejaz98@gmail.com>
2019-05-07 05:40:11 +0500

    Update inference.py

--- UNet_Segmentation/inference.py
+++ UNet_Segmentation/inference.py
@@ -1,22 +1,23 @@
 import tensorflow as tf
 import cv2
-import network
-from scipy.ndimage.measurements import label
 import matplotlib.pyplot as plt
 import numpy as np
 
-X = tf.placeholder(tf.float32, shape=[None, 256, 256, 3], name="image")
-img_file = '1ETIS.tif'
-img = cv2.imread(img_file)
+tf.reset_default_graph()
+X = tf.placeholder(tf.float32, shape=[None, 256, 256, 1], name="image")
+img_file = 'CVC-ClinicDB/Ground Truth/66.tif'
+img = cv2.imread(img_file, 0)
 img = cv2.resize(img, (256, 256))
-pred = network.make_unet(X, False)
+plt.imshow(img, cmap='gray')
+plt.show()
+img = np.reshape(img, (256, 256, 1))
+pred = make_unet(X, False)
 saver = tf.train.Saver()
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     ckpt = tf.train.get_checkpoint_state("model")
-    saver.restore(sess, "model/model-epoch500.ckpt")
+    saver.restore(sess, "model/model-epoch600.ckpt")
     mask_pred = sess.run(pred, feed_dict={X:[img]})
     mask_pred = np.squeeze(mask_pred)
-    mask_pred = mask_pred > 0.9999
     plt.imshow(mask_pred, cmap='gray')
     plt.show()
