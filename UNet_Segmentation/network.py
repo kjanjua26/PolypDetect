75b692753848efe9d1c991ce8b69ba6c48de40f0
Kamran Janjua <kamranejaz98@gmail.com>
2019-05-07 05:40:52 +0500

    Update network.py

--- UNet_Segmentation/network.py
+++ UNet_Segmentation/network.py
@@ -1,49 +1,44 @@
 import tensorflow as tf
 
-def conv_conv_pool(input_, n_filters, training, flags, name, pool=True, activation=tf.nn.relu):
-    net = input_
-    with tf.variable_scope("layer{}".format(name)):
-        for i, F in enumerate(n_filters):
-            net = tf.layers.conv2d(net, F, (3, 3), activation=None, padding='same', kernel_regularizer=tf.contrib.layers.l2_regularizer(0.1), name="conv_{}".format(i + 1))
-            net = tf.layers.batch_normalization(
-                net, training=training, name="bn_{}".format(i + 1))
-            net = activation(net, name="relu{}_{}".format(name, i + 1))
-
-        if pool is False:
-            return net
-        pool = tf.layers.max_pooling2d(
-            net, (2, 2), strides=(2, 2), name="pool_{}".format(name))
-        return net, pool
-
-def upconv_concat(inputA, input_B, n_filter, flags, name):
-    up_conv = upconv_2D(inputA, n_filter, flags, name)
-    return tf.concat([up_conv, input_B], axis=-1, name="concat_{}".format(name))
-
-def upconv_2D(tensor, n_filter, flags, name):
-    return tf.layers.conv2d_transpose(tensor, filters=n_filter, kernel_size=2, strides=2, kernel_regularizer=tf.contrib.layers.l2_regularizer(0.1), name="upsample_{}".format(name))
-
-def make_unet(X, training, flags=None):
-    net = X / 127.5 - 1
-    conv1, pool1 = conv_conv_pool(net, [8, 8], training, flags, name=1)
-    conv2, pool2 = conv_conv_pool(pool1, [16, 16], training, flags, name=2)
-    conv3, pool3 = conv_conv_pool(pool2, [32, 32], training, flags, name=3)
-    conv4, pool4 = conv_conv_pool(pool3, [64, 64], training, flags, name=4)
-    conv5 = conv_conv_pool(pool4, [128, 128], training, flags, name=5, pool=False)
-    up6 = upconv_concat(conv5, conv4, 64, flags, name=6)
-    conv6 = conv_conv_pool(up6, [64, 64], training, flags, name=6, pool=False)
-    up7 = upconv_concat(conv6, conv3, 32, flags, name=7)
-    conv7 = conv_conv_pool(up7, [32, 32], training, flags, name=7, pool=False)
-    up8 = upconv_concat(conv7, conv2, 16, flags, name=8)
-    conv8 = conv_conv_pool(up8, [16, 16], training, flags, name=8, pool=False)
-    up9 = upconv_concat(conv8, conv1, 8, flags, name=9)
-    conv9 = conv_conv_pool(up9, [8, 8], training, flags, name=9, pool=False)
-    return tf.layers.conv2d(conv9, 1, (1, 1), name='final', activation=tf.nn.sigmoid, padding='same')
-
+def make_unet(X, training):
+    input_layer = X / 127.5 - 1
+    conv1 = tf.layers.conv2d(inputs=input_layer, filters=8, kernel_size=[5, 5],padding="same",activation=tf.nn.relu,kernel_regularizer=tf.contrib.layers.l2_regularizer(0.1), name="conv_layer_1")
+    bn1 = tf.layers.batch_normalization(conv1, training=training, name="bn_1")
+    pool1 = tf.layers.max_pooling2d(bn1, (2, 2), strides=(2, 2), name="pool_1")
+    conv2 = tf.layers.conv2d(inputs=pool1, filters=16, kernel_size=[5, 5],padding="same",activation=tf.nn.relu,kernel_regularizer=tf.contrib.layers.l2_regularizer(0.1), name="conv_layer_2")
+    bn2 = tf.layers.batch_normalization(conv2, training=training, name="bn_2")
+    pool2 = tf.layers.max_pooling2d(bn2, (2, 2), strides=(2, 2), name="pool_2")
+    conv3 = tf.layers.conv2d(inputs=pool2, filters=32, kernel_size=[5, 5],padding="same",activation=tf.nn.relu,kernel_regularizer=tf.contrib.layers.l2_regularizer(0.1), name="conv_layer_3")
+    bn3 = tf.layers.batch_normalization(conv3, training=training, name="bn_3")
+    pool3 = tf.layers.max_pooling2d(bn3, (2, 2), strides=(2, 2), name="pool_3")
+    conv4 = tf.layers.conv2d(inputs=pool3, filters=64, kernel_size=[5, 5],padding="same",activation=tf.nn.relu,kernel_regularizer=tf.contrib.layers.l2_regularizer(0.1), name="conv_layer_4")
+    bn4 = tf.layers.batch_normalization(conv4, training=training, name="bn_4")
+    pool4 = tf.layers.max_pooling2d(bn4, (2, 2), strides=(2, 2), name="pool_4")
+    conv5 = tf.layers.conv2d(inputs=pool4, filters=128, kernel_size=[5, 5],padding="same",activation=tf.nn.relu,kernel_regularizer=tf.contrib.layers.l2_regularizer(0.1), name="conv_layer_5")
+    bn5 = tf.layers.batch_normalization(conv5, training=training, name="bn_5")
+    up_conv6 = tf.layers.conv2d_transpose(bn5, filters=64, kernel_size=2, strides=2, kernel_regularizer=tf.contrib.layers.l2_regularizer(0.1), name="upconv_6")
+    concat6 = tf.concat([up_conv6, conv4], axis=-1, name="concat_6")
+    conv6 = tf.layers.conv2d(inputs=concat6, filters=32, kernel_size=[5, 5],padding="same",activation=tf.nn.relu,kernel_regularizer=tf.contrib.layers.l2_regularizer(0.1), name="conv_layer_6") 
+    bn6 = tf.layers.batch_normalization(conv6, training=training, name="bn_6")
+    up_conv7 = tf.layers.conv2d_transpose(bn6, filters=32, kernel_size=2, strides=2, kernel_regularizer=tf.contrib.layers.l2_regularizer(0.1), name="upconv_7")
+    concat7 = tf.concat([up_conv7,conv3], axis=-1, name="concat_7")
+    conv7 = tf.layers.conv2d(inputs=concat7, filters=32, kernel_size=[5, 5],padding="same",activation=tf.nn.relu,kernel_regularizer=tf.contrib.layers.l2_regularizer(0.1), name="conv_layer_7")
+    bn7 = tf.layers.batch_normalization(conv7, training=training, name="bn_7")
+    up_conv8 = tf.layers.conv2d_transpose(bn7, filters=16, kernel_size=2, strides=2, kernel_regularizer=tf.contrib.layers.l2_regularizer(0.1), name="upconv_8")
+    concat8 = tf.concat([up_conv8, conv2], axis=-1, name="concat_8")
+    conv8 = tf.layers.conv2d(inputs=concat8, filters=32, kernel_size=[5, 5],padding="same",activation=tf.nn.relu,kernel_regularizer=tf.contrib.layers.l2_regularizer(0.1), name="conv_layer_8")
+    bn8 = tf.layers.batch_normalization(conv8, training=training, name="bn_8")
+    up_conv9 = tf.layers.conv2d_transpose(bn8, filters=8, kernel_size=2, strides=2, kernel_regularizer=tf.contrib.layers.l2_regularizer(0.1), name="upconv_9")
+    concat9 = tf.concat([up_conv9, conv1], axis=-1, name="concat_9")
+    conv9 = tf.layers.conv2d(inputs=concat9, filters=32, kernel_size=[5, 5],padding="same",activation=tf.nn.relu,kernel_regularizer=tf.contrib.layers.l2_regularizer(0.1), name="conv_layer_9")
+    bn9 = tf.layers.batch_normalization(conv9, training=training, name="bn_9")
+    out = tf.layers.conv2d(bn9, 1, (1, 1), name='final', activation=tf.nn.sigmoid, padding='same')
+    return out
 
 def IOU_(y_pred, y_true):
     H, W, _ = y_pred.get_shape().as_list()[1:]
     pred_flat = tf.reshape(y_pred, [-1, H * W])
     true_flat = tf.reshape(y_true, [-1, H * W])
-    intersection = tf.reduce_sum(pred_flat * true_flat, axis=1) + 1e-7
+    intersection = 10*tf.reduce_sum(pred_flat * true_flat, axis=1) + 1e-7
     union = tf.reduce_sum(pred_flat, axis=1) + tf.reduce_sum(true_flat, axis=1) + 1e-7
     return tf.reduce_mean(intersection / union)
