a2e693a5cc385f1bb63e3e1d02828abf46e9b28d
Kamran Janjua <kamranejaz98@gmail.com>
2019-05-07 05:41:35 +0500

    Update train.py

--- UNet_Segmentation/train.py
+++ UNet_Segmentation/train.py
@@ -2,20 +2,21 @@ import tensorflow as tf
 import network
 import utils
 import os
+
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 NUM_EPOCHS = 500
 BATCH_SIZE = 32
 
-X = tf.placeholder(tf.float32, shape=[None, 256, 256, 3], name="image")
+X = tf.placeholder(tf.float32, shape=[None, 256, 256, 1], name="image")
 Y = tf.placeholder(tf.float32, shape=[None, 256, 256, 1], name="mask")
 isTraining = tf.placeholder(tf.bool, name="iftraining")
 global_step = tf.Variable(0, trainable=False, name='global_step')
 
 def build_network(input_images, mask_labels):
     logits = network.make_unet(input_images, isTraining)
-    loss = -network.IOU_(logits, mask_labels)
+    loss = 10-network.IOU_(logits, mask_labels)
     return loss
-    
+  
 def train(x_train, x_val, y_train, y_val):
     loss = build_network(X, Y)
     optimizer = tf.train.AdamOptimizer()
@@ -24,14 +25,14 @@ def train(x_train, x_val, y_train, y_val):
     sess = tf.Session()
     sess.run(tf.global_variables_initializer())
     saver = tf.train.Saver()
-    for epoch in range(1, NUM_EPOCHS):
+    for epoch in range(1, NUM_EPOCHS+1):
         batch_images, batch_labels = utils.next_batch(BATCH_SIZE, x_train, y_train)
         _, train_iou = sess.run([train_op, loss], feed_dict={X: batch_images, Y: batch_labels, isTraining: True})
-        print('Step: {} IOU: {}'.format(epoch, -1*train_iou))
+        print('Step: {} Loss: {}'.format(epoch, train_iou))
         if epoch % 100 == 0:
             val_iou = sess.run([loss], feed_dict={X: x_val, Y: y_val, isTraining: False})
             print("")
-            print("Step: {} Val IOU: {}".format(epoch, -1*val_iou))
+            print("Step: {} Loss: {}".format(epoch, val_iou[0]))
             save_path = saver.save(sess, "model/model-epoch{}.ckpt".format(epoch))
             print("Model saved for epoch # {}".format(epoch))
             print("")
